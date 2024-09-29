69f99f47d28829f3c0d8b33183f5e6de6ce685ed
AkulGumudavalli <gumudaau@mail.uc.edu>
2024-09-29 15:14:41 -0400

    Update Main.java
    Added pseudocode, and test.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int age = 0;
+        // num age = 0
+        //Output "What is your age"
         System.out.println("What is your age?");
-        int age = input.nextInt();
-        if(age > 20){
-            System.out.println("You get a wristband");
-
-        }
-
+        //Input age
+        if (input.hasNextInt()){
+            int age = input.nextInt();
+        //if age > 20 then 
+            if(age > 20){
+                //Output you get a wristband
+                System.out.println("You get a wristband");
+            }
+        //end if 
+        }   
     }
 }
