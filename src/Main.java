6e8760bfdb97b5f0294c90f6e1ff1f530f1c7490
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-29 15:29:15 -0400

    fixed errors

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int age = 0;
+        int age;
         // num age = 0
         //Output "What is your age"
         System.out.println("What is your age?");
         //Input age
         if (input.hasNextInt()){
-            int age = input.nextInt();
+            age = input.nextInt();
         //if age > 20 then 
             if(age > 20){
                 //Output you get a wristband
